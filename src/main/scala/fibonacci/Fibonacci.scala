4493cf55697babc08cde283ced71bc22760632a6
Youngho Andrew Chaa <andrew.yh.chaa@gmail.com>
2025-10-03 23:11:42 +0100

    fibonacci

--- src/main/scala/fibonacci/Fibonacci.scala
+++ src/main/scala/fibonacci/Fibonacci.scala
@@ -2,5 +2,10 @@ package fibonacci
 
 @main def run(): Unit = {
   println("Hello from Fibonacci!")
-  // Your code here
+  println(s"fibonacci(10) = ${fibonacci(10)}")
+}
+
+def fibonacci(n: Int): Int = {
+  if (n <= 1) n
+  else fibonacci(n - 1) + fibonacci(n - 2)
 }
